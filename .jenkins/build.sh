a3e4301392fc895eb2c4496196b19c197d5313d6
Yaroslav Pshenichnikov <yaroslav.pshenichnikov@onlyoffice.com>
2025-03-10 14:44:06 +0300

    Modify deploy scripts

--- .jenkins/build.sh
+++ .jenkins/build.sh
@@ -1,5 +1,7 @@
 #!/bin/bash
 
+### Settings
+
 # Timestamp
 CURRENT_DATE=$(date "+%Y%m%d-%H%M%S")
 
@@ -8,35 +10,50 @@ DOCKER_CONTAINER_TAG="node:16-alpine"
 EXPOSE_PORT="30006"
 APP_NAME="helpcenter"
 APP_DIR="/app/$APP_NAME"
+BUILD_DIR="/app/${APP_NAME}_BUILD"
 SOURCE_ARCHIVE_PATH="/home/ubuntu/deploy/.jenkins/$APP_NAME.tar.gz"
 BACKUP_DIR="/app/backups"
 BACKUP_NAME="$APP_NAME-$CURRENT_DATE.tar.gz"
 
-# Stop application container if it exists
-if docker ps -a | grep -wq "$APP_NAME"; then
-    docker stop "$APP_NAME"
+### BUILD STAGE
+
+# Create folder with sources and extract sources to it
+mkdir -p "$BUILD_DIR"
+tar -xzf "$SOURCE_ARCHIVE_PATH" -C "$BUILD_DIR"
+find "$BUILD_DIR" -type d -exec chmod 755 {} \;
+find "$BUILD_DIR" -type f -exec chmod 644 {} \;
+cd "$BUILD_DIR"
+
+# Build application with error handling
+echo "Starting application build..."
+docker run --rm -v "$BUILD_DIR":"$APP_DIR" -w "$APP_DIR" "$DOCKER_CONTAINER_TAG" sh -c "yarn && yarn build"
+if [ $? -ne 0 ]; then
+    echo "Error: Application build failed. Cleaning up build directory and exiting."
+    rm -rf "$BUILD_DIR"
+    exit 1
 fi
+echo "Application build completed successfully."
+
+### DEPLOY STAGE
 
 # Archive the folder with date-time stamp
 tar -czf "$BACKUP_DIR/$BACKUP_NAME" -C "$APP_DIR" .
 
 # Cleanup other www-cms-marketplace backups to leave only 7 latest
 cd "$BACKUP_DIR"
-ls -1t "$APP_NAME-*.tar.gz" | tail -n +8 | xargs -I {} rm -- {}
+ls -1t $APP_NAME-*.tar.gz | tail -n +8 | xargs -I {} rm -- {}
+
+# Stop application container if it exists
+if docker ps -a | grep -wq "$APP_NAME"; then
+    docker stop "$APP_NAME"
+fi
 
 # Remove the original /app/www-cms-marketplace folder
 rm -rf "$APP_DIR"
 
-# Create folder with sources and extract sources to it
-mkdir -p "$APP_DIR"
-tar -xzf "$SOURCE_ARCHIVE_PATH" -C "$APP_DIR"
-find "$APP_DIR" -type d -exec chmod 755 {} \;
-find "$APP_DIR" -type f -exec chmod 644 {} \;
-cd "$APP_DIR"
+# Copy built app to app dir
+mv $BUILD_DIR $APP_DIR
 
-# Build application
-docker run --rm -v "$APP_DIR":"$APP_DIR" -w "$APP_DIR" "$DOCKER_CONTAINER_TAG" sh -c "yarn && yarn build"
- 
 # Start container if it exists or build and start it
 if docker ps -a | grep -wq "$APP_NAME"; then
     # Get the image tag of the existing container
@@ -51,6 +68,6 @@ if docker ps -a | grep -wq "$APP_NAME"; then
         docker run -d --name "$APP_NAME" --publish "0.0.0.0:$EXPOSE_PORT:$EXPOSE_PORT" -v "$APP_DIR:$APP_DIR" -w "$APP_DIR" --restart always "$DOCKER_CONTAINER_TAG" yarn start
     fi
 else
-        # Build and run a new container if it doesn't exist
-        docker run -d --name "$APP_NAME" --publish "0.0.0.0:$EXPOSE_PORT:$EXPOSE_PORT" -v "$APP_DIR:$APP_DIR" -w "$APP_DIR" --restart always "$DOCKER_CONTAINER_TAG" yarn start
+    # Build and run a new container if it doesn't exist
+    docker run -d --name "$APP_NAME" --publish "0.0.0.0:$EXPOSE_PORT:$EXPOSE_PORT" -v "$APP_DIR:$APP_DIR" -w "$APP_DIR" --restart always "$DOCKER_CONTAINER_TAG" yarn start
 fi
