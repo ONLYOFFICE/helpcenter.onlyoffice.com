9a68d8e2a5545582020f49492544373b440d0b59
Vyacheslav Belov <vyacheslav.belov@onlyoffice.com>
2025-06-05 16:42:25 +0300

    Update next.config.js

--- next.config.js
+++ next.config.js
@@ -2,16 +2,41 @@ const { i18n } = require("./next-i18next.config");
 const redirects = require('./config/redirects.json');
 
 const nextConfig = {
+  
+  generateBuildId: async () => {
+    return `build-${Date.now()}`;
+  },
+  
+  assetPrefix: process.env.NODE_ENV === 'production' ? `/_next` : '',
   async headers() {
     return [
       {
-        source: '/:all*(svg|jpg|png)',
+        source: '/:all*(js)',
         locale: false,
         headers: [
           {
             key: 'Cache-Control',
-            value: 'public, max-age=31536000 , must-revalidate',
+            value: 'public, max-age=0, must-revalidate',
           }
+        ],
+      },
+      {
+        source: '/:all*(svg|jpg|png|css)',
+        locale: false,
+        headers: [
+          {
+            key: 'Cache-Control',
+            value: 'public, max-age=31536000, must-revalidate',
+          }
+        ],
+      },
+      {
+        source: '/_next/static/(.*)',
+        headers: [
+          {
+            key: 'Cache-Control',
+            value: 'public, max-age=31536000, immutable',
+          },
         ],
       },
       {
