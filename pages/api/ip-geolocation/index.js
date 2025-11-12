e9ac350923904be6cd45c0bfc970c104eb933e33
juli vytnova <maystrovich.julia@onlyoffice.com>
2025-11-12 16:59:03 +0300

    fixes

--- pages/api/ip-geolocation/index.js
+++ pages/api/ip-geolocation/index.js
@@ -11,7 +11,6 @@ export default async function handler(req, res) {
       method: req.method,
       headers: {
         "Content-Type": "application/json",
-        ...req.headers,
       },
     });
 
