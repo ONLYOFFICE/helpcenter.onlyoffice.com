c74f9ab4caa7ea8a90b1f027d67a276d384052df
juli vytnova <maystrovich.julia@onlyoffice.com>
2026-02-26 17:40:50 +0300

    add copy link button

--- components/screens/article-content/styled-article-content.js
+++ components/screens/article-content/styled-article-content.js
@@ -4,6 +4,22 @@ import Section from "@components/common/section";
 const StyledArticleContent = styled(Section)`
   .section-page {
     display: flex;
+
+    .anchor-copy-btn {
+      border: none;
+      padding: 0;
+      width: 24px;
+      height: 24px;
+      background-image: url(/_next/static/media/copy.e7f8d68a.svg);
+      background-repeat: no-repeat;
+      background-size: 12px 12px;
+      background-position: center;
+      background-color: transparent;
+      cursor: pointer;
+      position: relative;
+      top: 5px;
+      right: 0;
+    }
   }
 `;
 
