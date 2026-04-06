21c8498d944c18590e05577aeb1b1693beb0c1d2
juli vytnova <maystrovich.julia@onlyoffice.com>
2026-04-06 14:52:16 +0300

    Update treeview-data.js

--- components/screens/common/left-menu/sub-components/treeview/treeview-data.js
+++ components/screens/common/left-menu/sub-components/treeview/treeview-data.js
@@ -1,46 +1,57 @@
 const GenerateTreeViewData = (data) => {
-  const sortByIconOrPositionOrNameTitle = (a, b) => (b.icon_small?.data?.url ? 1 : 0) - (a.icon_small?.data?.url ? 1 : 0) || (a.position ?? Infinity) - (b.position ?? Infinity) || (a.name || a.title).localeCompare(b.name || b.title);
-  const sortByPositionOrTitle = (a, b) => (a.position ?? Infinity) - (b.position ?? Infinity) || (a.title).localeCompare(b.title);
+  const sortByPositionOrNameTitle = (a, b) =>
+    (a.position ?? Infinity) - (b.position ?? Infinity) ||
+    (a.name || a.title || "").localeCompare(b.name || b.title || "");
 
   return {
-    children: data?.data?.sort(sortByIconOrPositionOrNameTitle).map((item) => {
+    children: data?.data?.sort(sortByPositionOrNameTitle).map((item) => {
       const slug_id = `${item?.slug_id === "docs" ? "docs" : `${item?.slug_id}s`}`;
 
       return {
         name: item?.name,
         slug_id: item?.slug_id,
-        children: [...(item?.[`category_${slug_id}`] ?? []), ...(item?.articles ?? [])]?.sort(sortByIconOrPositionOrNameTitle).map((item2) => ({
-          name: item2?.name || item2?.title,
-          url: item2?.url,
-          children: [...(item2?.[`level_2_${slug_id}`] ?? []), ...(item2?.[`article_${slug_id}`] ?? [])].sort(sortByIconOrPositionOrNameTitle).map((item3) => ({
-            name: item3?.name || item3?.title,
-            url: item3?.url,
+        children: [
+          ...(item?.[`category_${slug_id}`] ?? []),
+          ...(item?.articles ?? []),
+        ]
+          .sort(sortByPositionOrNameTitle)
+          .map((item2) => ({
+            name: item2?.name || item2?.title,
+            url: item2?.url,
             children: [
-              ...(item3?.[`level_3_${slug_id}`] ?? [])?.sort(sortByIconOrPositionOrNameTitle).map((item4) => ({
-                name: item4?.name || item4?.title,
-                url: item4?.url,
+              ...(item2?.[`level_2_${slug_id}`] ?? []),
+              ...(item2?.[`article_${slug_id}`] ?? []),
+            ]
+              .sort(sortByPositionOrNameTitle)
+              .map((item3) => ({
+                name: item3?.name || item3?.level_4_title || item3?.title,
+                url: item3?.url,
                 children: [
-                  ...(item4?.[`level_4_${slug_id}`] ?? []).sort(sortByIconOrPositionOrNameTitle).map((item5) => ({
-                    name: item5?.name || item5?.title,
-                    url: item5?.url,
-                    children: item5?.[`article_${slug_id}`]?.data?.sort(sortByPositionOrTitle).map((item6) => ({
-                      name: item6?.name || item6?.title,
-                      url: item6?.url,
-                    }))
-                  })),
-                  ...(item4?.[`article_${slug_id}`] ?? [])?.sort(sortByPositionOrTitle).map((item5) => ({
-                    name: item5?.name || item5?.level_4_title || item5?.title,
-                    url: item5?.url,
-                  })),
+                  ...(item3?.[`level_3_${slug_id}`] ?? []),
+                  ...(item3?.[`article_${slug_id}`] ?? []),
                 ]
+                  .sort(sortByPositionOrNameTitle)
+                  .map((item4) => ({
+                    name: item4?.name || item4?.level_4_title || item4?.title,
+                    url: item4?.url,
+                    children: [
+                      ...(item4?.[`level_4_${slug_id}`] ?? []),
+                      ...(item4?.[`article_${slug_id}`] ?? []),
+                    ]
+                      .sort(sortByPositionOrNameTitle)
+                      .map((item5) => ({
+                        name: item5?.name || item5?.level_4_title || item5?.title,
+                        url: item5?.url,
+                        children: item5?.[`article_${slug_id}`]
+                          ?.sort(sortByPositionOrNameTitle)
+                          .map((item6) => ({
+                            name: item6?.name || item6?.level_4_title || item6?.title,
+                            url: item6?.url,
+                          })),
+                      })),
+                  })),
               })),
-              ...(item3?.[`article_${slug_id}`] ?? [])?.sort(sortByPositionOrTitle).map((item4) => ({
-                name: item4?.name || item4?.level_4_title || item4?.title,
-                url: item4?.url,
-              })),
-            ],
           })),
-        })),
       };
     }),
   };
